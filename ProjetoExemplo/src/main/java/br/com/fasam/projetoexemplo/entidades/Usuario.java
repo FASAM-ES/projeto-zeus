b88e8c542672c68c17b9d26d8bc086982a5582bc
Aluno <Aluno@172.16.1.39>
2014-11-29 11:20:36 -0200

    Add Get e Set na Classe Usuario

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Usuario.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Usuario.java
@@ -15,5 +15,53 @@ public class Usuario {
     String senha;
     String telefone;
     String cpf;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
     
 }
