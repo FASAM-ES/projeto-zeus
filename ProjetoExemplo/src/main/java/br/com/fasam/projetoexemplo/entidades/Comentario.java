19bed4f8c1ea34a09629503bb21406e8ed41b218
Aluno <Aluno@172.16.1.28>
2014-11-29 11:32:02 -0200

    Adicionou Classe Comentario.

---
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Comentario.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fasam.projetoexemplo.entidades;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Comentario {
+   String titulo;
+   String descricao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+          
+}
