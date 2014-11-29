ac687c8e964f2bd6d81adebe700c25e952103c72
Aluno <Aluno@172.16.1.42>
2014-11-29 10:42:26 -0200

    Incluindo pacote entidades, e classe Artigo

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Consegui commitar!!!23! Pedro"  );
+        System.out.println( "Hello packages"  );
     }
 }
