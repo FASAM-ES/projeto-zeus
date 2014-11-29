24b4e581287b07fec25cd60caa9afb32b3e98be2
jcottobboni <jcottobboni@gmail.com>
2014-11-29 16:11:59 -0200

    Criando Teste para conexão

---
+++ ProjetoExemplo/src/test/java/br/com/fasam/projetoexemplo/ConexaoTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fasam.projetoexemplo;
+
+import br.com.fasam.conexao.Conexao;
+import java.sql.Connection;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ConexaoTest extends TestCase {
+    
+    public ConexaoTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+    public void testConexao(){
+        Connection con = Conexao.getConexao();
+        assertNotNull(con);
+    }
+}
