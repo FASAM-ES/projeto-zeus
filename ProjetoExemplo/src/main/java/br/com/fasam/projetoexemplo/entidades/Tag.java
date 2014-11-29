237591eaa0fdb1e7fdf8c5bb1369d8aca3281863
Aluno <Aluno@172.16.1.106>
2014-11-29 11:19:54 -0200

    Adicionada classe tag

---
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Tag.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fasam.projetoexemplo.entidades;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Tag {
+    String nome;
+    String descricao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
