a4685dd4dbac6a7418bf95efb0fa3a3c55263a51
Aluno <Aluno@172.16.1.96>
2014-11-29 12:28:28 -0200

    Adicionando categoria - informatica

---
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Categoria.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fasam.projetoexemplo.entidades;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Categoria {
+    String nome; 
+    String descrição;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+    
+}
