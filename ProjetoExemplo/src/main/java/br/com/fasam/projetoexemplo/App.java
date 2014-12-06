d7cc7a56a4465f6433d4bf9d59717b73dec5a97e
Aluno <Aluno@172.16.1.42>
2014-12-06 13:56:55 -0200

    testando webhook

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello packages teste dois teste"  );
+        System.out.println( "Hello packages teste teste dois teste"  );
     }
 }
