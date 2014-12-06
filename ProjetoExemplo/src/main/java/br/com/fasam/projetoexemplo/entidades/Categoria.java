af3f7d6781ed318433a523c9b712dd2d1d2f5386
Anoar <anoarfilho@gmail.com>
2014-12-06 15:32:39 -0200

    Documentacao entiades.Categoria.java

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Categoria.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Categoria.java
@@ -13,35 +13,66 @@ public class Categoria {
     String descricao;
     List<Artigo> artigos;
     
+    /**
+     * Metodo inicializar a variavel nome
+     */
     public Categoria(){
     this.nome = "TI";
     }
+    /**
+     * Metodo de retorno artigos
+     * @param i
+     * @return 
+     */
      public Artigo getArtigo(Integer i){
         return artigos.get(i);
     }
+     /**
+      * Metodo de criacao da lista de artigos
+      */
     public void addArtigo(){
         if(this.artigos == null){
             this.artigos = new ArrayList<Artigo>();
         }
     }
+    /**
+     * Metodo para limpar a variavel artigos
+     * @param artigo 
+     */
     public void remArtigo(Artigo artigo){
         if(this.artigos != null){
             this.artigos.remove(artigo);
         }
     }
 
+    /**
+     * Metodo de retorno nome
+     * @return 
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Metodo para setar nome
+     * @param nome 
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Metodo de retorno descricao
+     * @return 
+     */
     public String getDescricao() {
         return descricao;
     }
 
+    /**
+     * Metodo para setar descricao
+     * @param descricao 
+     */
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
