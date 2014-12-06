81848d60a6732c2aaa1912097836a42edf26d3be
Aluno <Aluno@172.16.1.42>
2014-12-06 14:30:43 -0200

    removendo erros

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
@@ -4,8 +4,7 @@ package br.com.fasam.projetoexemplo;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ){
         System.out.println( "Hello packages sssteste teste teste dois teste"  );
     }
