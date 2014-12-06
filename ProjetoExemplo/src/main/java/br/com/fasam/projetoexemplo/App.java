e0ed48e87981c8dc3f8477b3560b04b74d01fb1f
Aluno <Aluno@172.16.1.39>
2014-12-06 14:22:03 -0200

    Arrumando bugs apontados pelo sonar

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
@@ -6,8 +6,7 @@ package br.com.fasam.projetoexemplo;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
         System.out.println( "Hello packages sssteste teste teste dois teste"  );
     }
 }
