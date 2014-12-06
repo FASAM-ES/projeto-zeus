63ce6944a5013b43800775ecda9199ea4a076502
jcottobboni <jcottobboni@gmail.com>
2014-12-06 15:34:31 -0200

    Adicionando docs

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Comentario.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/entidades/Comentario.java
@@ -15,6 +15,9 @@ public class Comentario {
     titulo = "Facebook";
     descricao = "tudo por aqui";
 }
+     /**
+     * Cosntrutor override
+     */
    
 
   
@@ -23,34 +26,58 @@ public class Comentario {
     public Usuario getUsuario() {
         return usuario;
     }
+     /**
+     * Retorna usuario
+     */
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
+     /**
+     * seta usuario
+     */
 
     public Artigo getArtigo() {
         return artigo;
     }
+     /**
+     * Retorna artigo
+     */
 
     public void setArtigo(Artigo artigo) {
         this.artigo = artigo;
     }
+     /**
+     * Seta  artigo
+     */
    
 
     public String getTitulo() {
         return titulo;
     }
+     /**
+     * Retorna titulo
+     */
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+     /**
+     * Seta titulo
+     */
 
     public String getDescricao() {
         return descricao;
     }
+     /**
+     * Retorna descricao
+     */
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+     /**
+     * set descricao
+     */
           
 }
