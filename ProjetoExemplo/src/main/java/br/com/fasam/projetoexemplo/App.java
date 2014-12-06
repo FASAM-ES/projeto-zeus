b33c5daa73f052faec8b62e8ec5316d9bf9b6377
Aluno <Aluno@172.16.1.42>
2014-12-06 14:18:52 -0200

    testando nexus

--- ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
+++ ProjetoExemplo/src/main/java/br/com/fasam/projetoexemplo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello packages teste teste teste dois teste"  );
+        System.out.println( "Hello packages sssteste teste teste dois teste"  );
     }
 }
